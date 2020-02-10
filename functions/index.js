5188fa8f89e1008fcd8b32564cc209fc0217b22e
Mario ANDRIANARILIVA <mario.andrianariliva@axian-group.com>
2020-02-10 19:36:19 +0100

    sign up function

--- functions/index.js
+++ functions/index.js
@@ -58,4 +58,30 @@ app.post('/scream',(req, res) => {
         })
  });
 
+ //Sign up user
+
+ app.post('/signup', (req,res) => {
+     const newUser = {
+         email: req.body.email,
+         password: req.body.password,
+         consfirmPassword: req.body.confirmPassword,
+         handle: req.body.handle
+     }
+
+     // TODO validate data
+
+     firebase
+        .auth()
+        .createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then((data) => {
+            return res
+                .status(201)
+                .json({ message: `user ${data.user.uid} signed up successfully`});
+        })
+        .catch((err) => {
+            console.err(err);
+            return res.status(500).json({ error : err.code});
+        });
+ })
+
  exports.api = functions.https.onRequest(app);
